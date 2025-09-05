96d830969160858f5ab645351bda0832062ec2cf
jemila2 <abubakarjemiletu08@gmail.com>
2025-09-06 00:35:18 +0100

    Delete src directory

--- src/components/Table.jsx
+++
@@ -1,9 +0,0 @@
-const Table = ({ children, className = '', ...props }) => {
-  return (
-    <table className={`min-w-full divide-y divide-gray-200 ${className}`} {...props}>
-      {children}
-    </table>
-  );
-};
-
-export default Table;
--- src/components/TaskList.jsx
+++
@@ -1,7 +0,0 @@
-const TaskList = ({ tasks, onTaskComplete }) => {
-  return (
-    <div className="task-list pl-50">
-    </div>
-  );
-};
-export default TaskList
