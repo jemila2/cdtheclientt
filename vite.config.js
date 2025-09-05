0ee472451753487d2632682d1757cdc76ce6fea5
jemila <abubakarjemiletu08@gmail.com>
2025-09-05 05:33:43 +0100

    changes made

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cdtheclienttt/', // Make sure this matches your GitHub repo name
+  base: '/cdtheclientt/', // Make sure this matches your GitHub repo name
   plugins: [
     react(),
   ],
