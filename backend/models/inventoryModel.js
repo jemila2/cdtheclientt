cf446e63df7299a0865ad268e542fdf4449e3920
jemila2 <abubakarjemiletu08@gmail.com>
2025-09-06 00:47:54 +0100

    Delete backend directory

--- backend/models/inventoryModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const inventorySchema = new mongoose.Schema({
-  name: String,
-  quantity: Number,
- 
-});
-
-module.exports = mongoose.model('Inventory', inventorySchema);
